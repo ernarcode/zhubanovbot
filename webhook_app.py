48fd86780bfaedfd60a658fa7ef651e0432d1fed
ernarcode <126081154+ernarcode@users.noreply.github.com>
2025-06-19 12:50:08 +0500

    Create webhook_app.py

---
+++ webhook_app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.types import Update
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.client.default import DefaultBotProperties
+
+from handlers import router  # или другой файл с хендлерами
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+app = Flask(__name__)
+dp = Dispatcher()
+dp.include_router(router)
+
+session = AiohttpSession()
+bot = Bot(
+    token=TOKEN,
+    session=session,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    update = Update.model_validate_json(request.data)
+    asyncio.run(dp.feed_update(bot=bot, update=update))
+    return "ok", 200
+
+@app.route("/set_webhook", methods=["GET"])
+def set_webhook():
+    url = os.getenv("WEBHOOK_URL")
+    asyncio.run(_set_webhook(url))
+    return f"Webhook установлен на {url}"
+
+async def _set_webhook(url):
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_webhook(url)
